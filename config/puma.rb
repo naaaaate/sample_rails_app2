b8ceb1540b652169636d9c2eaf018021d32ba4bb
Nathan Lee <nlee43@gmail.com>
2015-09-28 11:49:28 -0700

    Use SSL and the Puma webserver in production

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+# also create a procfile in root
+# it tells Heroku to run a Puma process in production
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
